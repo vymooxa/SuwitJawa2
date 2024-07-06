e3289a61487bcc85bc10c23265199e018e29d155
vymooxa <betari.ambarukmi@gmail.com>
2024-07-06 13:26:03 +0700

    WPU Version

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-var tanya = true;
-while (tanya) {
-  //menangkap pilihan player
-  var p = prompt("pilih : gajah, semut,orang");
+function getPilihanComputer() {
+  const comp = Math.random();
 
-  //menangkap pilhan computer
-  var comp = Math.random();
-
-  if (comp < 0.34) {
-    comp = "gajah";
-  } else if (comp > 0.34 && comp < 0.67) {
-    comp = "orang";
-  } else {
-    comp = "semut";
-  }
-
-  //menentukan rules
-  var hasil = "";
-  if (p == comp) {
-    hasil = "SERI";
-  } else if (p == "gajah") {
-    hasil = comp == "orang" ? "MENANG" : "KALAH";
-  } else if (p == "orang") {
-    hasil = comp == "semut" ? "MENANG" : "KALAH";
-  } else if (p == "semut") {
-    hasil = comp == "gajah" ? "KALAH" : "MENANG";
-  } else {
-    hasil = "memasukkan pilihan yang salah";
-  }
-
-  // tampilkan hasilnya
-  alert(
-    "Kamu memilih : " +
-      p +
-      " dan Komputer memilih : " +
-      comp +
-      "\nMaka hasilnya : Kamu " +
-      hasil
-  );
-
-  tanya = confirm("lagi?");
+  if (comp < 0.34) return "gajah";
+  if (comp >= 0.34 && comp < 0.67) return "orang";
+  return "semut";
 }
 
+function getHasil(comp, player) {
+  if (player == comp) return "SERI";
+  if (player == "gajah") return comp == "orang" ? "MENANG" : "KALAH";
+  if (player == "orang") return comp == "gajah" ? "KALAH" : "MENANG";
+  if (player == "semut") return comp == "orang" ? "KALAH" : "MENANG";
+}
+
+function putar() {
+  const imgComputer = document.querySelector(".img-komputer");
+  const gambar = ["gajah", "semut", "orang"];
+  let i = 0;
+  const waktuMulai = new Date().getTime();
+  setInterval(function () {
+    if (new Date().getTime() - waktuMulai > 1000) {
+      clearInterval;
+      return;
+    }
+    imgComputer.setAttribute("src", "img/" + gambar[i++] + ".png");
+    if (i == gambar.length) i = 0;
+  }, 100);
+}
+
+const pPlayer = document.querySelectorAll("li img");
+pPlayer.forEach(function (pil) {
+  pil.addEventListener("click", function () {
+    const pilihanComputer = getPilihanComputer();
+    const pilihanPlayer = pil.className;
+    const hasil = getHasil(pilihanComputer, pilihanPlayer);
+
+    putar();
+
+    setTimeout(function () {
+      const imgComputer = document.querySelector(".img-komputer");
+      imgComputer.setAttribute("src", "img/" + pilihanComputer + ".png");
+      console.log("comp : " + pilihanComputer);
+      console.log("player :" + pilihanPlayer);
+      console.log("hasil :" + hasil);
+
+      const info = document.querySelector(".info");
+      info.innerHTML = hasil;
+    }, 1000);
+  });
+});
